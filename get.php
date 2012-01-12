274eab0fc515db2778d26a4907faddd3db3bd9b1
Julien Brochet <mewt@madalynn.eu>
2012-01-12 15:47:55 +0100

    it's working!

--- get.php
+++ get.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+ * (c) 2012 Julien Brochet <mewt@madalynn.eu>
+ */
+
 require_once __DIR__.'/vendor/.composer/autoload.php';
 
 if (3 !== count($argv)) {
@@ -50,3 +54,39 @@ if (0 === preg_match('/window.location.href="https:\/\/tomuss.univ-lyon1.fr\/\?t
     echo "Oops! Unable to find the token from the CAS...\n";
     exit;
 }
+
+$token = $matches[1];
+
+// ####################################
+//    Tomuss
+// ####################################
+
+// Big hack: The curl PHP API returns 0 bytes with this URL
+$output = exec('curl -L https://tomuss.univ-lyon1.fr/?ticket='.$token.' 2> /dev/null');
+
+$regex = '/C\(([0-9]+([.][0-9]+)?),"([A-Za-z.]+)","([0-9]{14})"\)/';
+
+if (0 === preg_match_all($regex, $output, $matches, PREG_SET_ORDER)) {
+    // No results
+    exit;
+}
+
+foreach($matches as $note) {
+    if (isset($notes[$note[4]])) {
+        // Note alreay exists
+        continue;
+    }
+
+    $notes[$note[4]] = array(
+        'by'   => $note[3],
+        'note' => $note[1]
+    );
+
+    echo sprintf('Nouvelle note: %s par %s'.PHP_EOL, $note[1], $note[3]);
+
+    // Comment this line if you're not a mac osx user using growl
+    exec(sprintf('growlnotify -t "Nouvelle note Tomuss" -n "Tomuss" -m "%s postée par %s"', $note[1], $note[3]));
+}
+
+// dump!
+file_put_contents($notesFile, serialize($notes));
